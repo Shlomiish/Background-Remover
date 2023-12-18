9930394a15dc4f8f27b600d5032fb161f90e571f
Shlomi Shoshani <shlomi.shosha@gmail.com>
2023-12-18 10:51:25 +0200

    change the base_url

--- src/api/endpoints/endpoints.ts
+++ src/api/endpoints/endpoints.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://photobox-background-remover-api-yjog.onrender.com';
+export const BASE_URL = 'http://localhost:5000';
 
 export const API_ENDPOINTS = {
   UPLOAD_IMAGE: `${BASE_URL}/upload_image`,
