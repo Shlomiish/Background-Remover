35faefca8100db0a969aa954fc0301fb297ac17f
Shlomi Shoshani <shlomi.shosha@gmail.com>
2023-12-21 09:48:03 +0200

    test

--- src/components/dashboard/Content/ImageViewer/ImageViewer.tsx
+++ src/components/dashboard/Content/ImageViewer/ImageViewer.tsx
@@ -18,33 +18,33 @@ export const ImageViewer: FC = () => {
   const canvasContext = useContext(CanvasContext);
 
   const uploadImage = (e: React.ChangeEvent<HTMLInputElement>) => {
-    // console.log('start');
+    console.log('start');
 
-    // const formData = new FormData();
-    // let file = e?.target?.files?.[0];
-    // if (file) {
-    //   console.log('after if');
-    //   formData.append('uploadedFile', file);
-    //   setIsLoader(true);
+    const formData = new FormData();
+    let file = e?.target?.files?.[0];
+    if (file) {
+      console.log('after if');
+      formData.append('uploadedFile', file);
+      setIsLoader(true);
 
-    //   try {
-    //     console.log('after try');
-    //     if (file?.type === 'image/jpeg' || file?.type === 'image/png') {
-    //       console.log('after try if');
+      try {
+        console.log('after try');
+        if (file?.type === 'image/jpeg' || file?.type === 'image/png') {
+          console.log('after try if');
 
-    //       API_REQUESTS.TEST_FUNC(formData).then((res) => {
-    //         canvasContext?.setImageFileNameFunc(res.data);
-    //         setIsLoader(false);
-    //       });
-    //     }
-    //   } catch (error) {
-    //     setIsLoader(false);
-    //     console.log(error);
-    //   }
-    // }
-    API_REQUESTS.TEST_FUNC().then((res) => {
-      console.log(res.data);
-    });
+          API_REQUESTS.TEST_FUNC(formData).then((res) => {
+            canvasContext?.setImageFileNameFunc(res.data);
+            setIsLoader(false);
+          });
+        }
+      } catch (error) {
+        setIsLoader(false);
+        console.log(error);
+      }
+    }
+    // API_REQUESTS.TEST_FUNC().then((res) => {
+    //   console.log(res.data);
+    // });
   };
 
   const openFileInputByRef = () => {
